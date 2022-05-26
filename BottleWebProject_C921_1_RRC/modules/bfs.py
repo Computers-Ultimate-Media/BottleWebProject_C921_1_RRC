da3fe087e2fc2b0cec261903b956e3d92c5794dc
Максим Казанцев <m.p.rybakov@gmail.com>
2022-05-26 16:30:58 +0300

    Доработан алгоритм bfs

--- BottleWebProject_C921_1_RRC/modules/bfs.py
+++ BottleWebProject_C921_1_RRC/modules/bfs.py
@@ -1,5 +1,4 @@
 import collections
-from BottleWebProject_C921_1_RRC.modules.utils import print_matrix
 
 
 def bfs(matrix_in: list[list[int]], start: int) -> list[list[int]]:
@@ -13,17 +12,12 @@ def bfs(matrix_in: list[list[int]], start: int) -> list[list[int]]:
 
     to_visit.append(start)
     while not len(to_visit) == 0:
-        print("vis :" + str(visited))
-        print("to :" + str(to_visit))
-
         node = to_visit.popleft()
         visited.add(node)
-        print(node)
         for connid, conn in enumerate(matrix_in[node]):
             if connid not in visited and conn > 0:
                 to_visit.append(connid)
                 visited.add(connid)
                 matrix_out[node][connid] = conn
                 matrix_out[connid][node] = conn
-        print_matrix(matrix_out)
     return matrix_out
