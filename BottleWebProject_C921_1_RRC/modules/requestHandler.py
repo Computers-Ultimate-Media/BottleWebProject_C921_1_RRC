80e3f67061a1e7e18049ab53de67f76ae0551384
Максим Казанцев <m.p.rybakov@gmail.com>
2022-05-27 02:20:55 +0300

    Добавлен api обработки запросов

---
+++ BottleWebProject_C921_1_RRC/modules/requestHandler.py
@@ -0,0 +1,37 @@
+from BottleWebProject_C921_1_RRC.database import insert, select_one
+from BottleWebProject_C921_1_RRC.modules.bfs import bfs
+from BottleWebProject_C921_1_RRC.modules.fileConverter import json_to_matrix, matrix_to_json
+
+
+def handle_request(data: dict) -> dict:
+    alg_type = int(data.get("AlgType"))
+    graph_in = str(data["Graph"])
+
+    matrix_in: list[list[int]] = json_to_matrix(graph_in)
+    matrix_out: list[list[int]] = list[list[int]]()
+
+    if alg_type == 1:
+        start = int(data["StartNode"])
+        matrix_out = bfs(matrix_in, start)
+        pass
+    elif alg_type == 2:
+        pass
+    elif alg_type == 3:
+        pass
+    else:
+        raise Exception("Unknown type of algorith")
+
+    graph_out = matrix_to_json(matrix_out)
+    result = dict()
+    result["Graph"] = graph_out
+    return result
+
+
+def save_to_database(alg_type: int, input: str, output: str) -> int:
+    sql = "insert into bottle_db.requests (alg_type, input, output) values (%s, %s, %s);"
+    val = (alg_type, input, output)
+    insert(sql, val)
+
+    sql = "select last_insert_id()"
+    res = select_one(sql)
+    return res[0]
--- BottleWebProject_C921_1_RRC/routes.py
+++ BottleWebProject_C921_1_RRC/routes.py
@@ -22,24 +22,33 @@ def contact():
     )
 
 
-@route('/check_result', method='post')
-def check_result():
+@route('/calculate', method='post')
+def calculate_request():
     from json import dumps as json_dumps, loads as json_loads
+    from BottleWebProject_C921_1_RRC.modules.requestHandler import handle_request
 
-    data = request.body.getvalue().decode('utf-8')
-    data = json_loads(data)
+    try:
+        decoded_request = request.body.getvalue().decode('utf-8')
+        data: dict = json_loads(decoded_request)
+
+        response: dict = handle_request(data)
+
+        response["Status"] = 200
+        return json_dumps(response)
+    finally:
+        return json_dumps({"Status": 400})
 
 
 @post('/bfs', method='post')
 @post('/dfs', method='post')
 @post('/kruskal', method='post')
-def result():
+def result_page():
     return redirect('/result')
 
 
 @route('/bfs')
 @view('algorithms/bfs')
-def bfs():
+def bfs_page():
     return dict(
         title='bfs',
         year=datetime.now().year
@@ -48,7 +57,7 @@ def bfs():
 
 @route('/dfs')
 @view('algorithms/dfs')
-def dfs():
+def dfs_page():
     return dict(
         title='dfs',
         year=datetime.now().year
@@ -57,7 +66,7 @@ def dfs():
 
 @route('/kruskal')
 @view('algorithms/kruskal')
-def kruskal():
+def kruskal_page():
     return dict(
         title='kruskal',
         year=datetime.now().year
