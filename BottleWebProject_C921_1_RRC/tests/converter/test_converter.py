588adb9982d254b4551432d408f567c187f72f87
Максим Казанцев <m.p.rybakov@gmail.com>
2022-05-26 16:29:26 +0300

    Изменение структуры тестов

--- BottleWebProject_C921_1_RRC/tests/converter/test_converter.py
+++ BottleWebProject_C921_1_RRC/tests/converter/test_converter.py
@@ -5,12 +5,10 @@ from BottleWebProject_C921_1_RRC.modules.fileConverter import json_to_matrix, ma
 
 class TestFileConverter(unittest.TestCase):
     def test_converting_both_ways(self):
-        with open("json.txt") as f:
+        with open("graph_default.json") as f:
             json_in = f.read()
 
         matrix = json_to_matrix(json_in)
-        for idx, line in enumerate(matrix):
-            print(line)
 
         json_out = matrix_to_json(matrix)
 
