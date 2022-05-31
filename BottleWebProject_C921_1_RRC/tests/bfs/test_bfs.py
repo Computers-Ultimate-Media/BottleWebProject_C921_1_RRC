c6880065ce5ad81c486abe7598f05df426fc4b95
Максим Казанцев <m.p.rybakov@gmail.com>
2022-05-31 14:46:54 +0300

    Обновление тестов

--- BottleWebProject_C921_1_RRC/tests/bfs/test_bfs.py
+++ BottleWebProject_C921_1_RRC/tests/bfs/test_bfs.py
@@ -1,24 +1,41 @@
-import json
 import unittest
 
-from BottleWebProject_C921_1_RRC.modules.formatConverter import data_to_matrix
 from BottleWebProject_C921_1_RRC.modules.algorithms.bfs import bfs
 
 
 class TestBFS(unittest.TestCase):
     def test_no_error(self):
-        with open("C:\\Users\\max\\PycharmProjects\\BottleWebProject_C921_1_RRC\\BottleWebProject_C921_1_RRC\\tests\\bfs\\graph_bfs_default.json") as f:
-            text = f.read()
-            matrix = data_to_matrix(json.loads(text))
+        matrix = [[0, 1, 1, 1, 1, 0, 0, 0],
+                  [1, 0, 1, 1, 1, 0, 0, 1],
+                  [1, 1, 0, 1, 1, 1, 0, 0],
+                  [1, 1, 1, 0, 1, 0, 0, 0],
+                  [1, 1, 1, 1, 0, 0, 1, 0],
+                  [0, 0, 1, 0, 0, 0, 1, 1],
+                  [0, 0, 0, 0, 1, 1, 0, 0],
+                  [0, 1, 0, 0, 0, 1, 0, 0]]
         res = bfs(matrix, 0)
         self.assertIsNotNone(res)
 
     def test_find_spanning_tree(self):
-        with open("C:\\Users\\max\\PycharmProjects\\BottleWebProject_C921_1_RRC\\BottleWebProject_C921_1_RRC\\tests\\bfs\\graph_bfs_default.json") as f:
-            matrix = data_to_matrix(f.read())
-            res = bfs(matrix, 2)
-        with open("C:\\Users\\max\\PycharmProjects\\BottleWebProject_C921_1_RRC\\BottleWebProject_C921_1_RRC\\tests\\bfs\\graph_bfs_result.json") as f:
-            matrix_correct = data_to_matrix(f.read())
+        matrix = [[0, 1, 1, 1, 1, 0, 0, 0],
+                  [1, 0, 1, 1, 1, 0, 0, 1],
+                  [1, 1, 0, 1, 1, 1, 0, 0],
+                  [1, 1, 1, 0, 1, 0, 0, 0],
+                  [1, 1, 1, 1, 0, 0, 1, 0],
+                  [0, 0, 1, 0, 0, 0, 1, 1],
+                  [0, 0, 0, 0, 1, 1, 0, 0],
+                  [0, 1, 0, 0, 0, 1, 0, 0]]
+
+        res = bfs(matrix, 0)
+
+        matrix_correct = [[0, 1, 1, 1, 1, 0, 0, 0],
+                          [1, 0, 0, 0, 0, 0, 0, 1],
+                          [1, 0, 0, 0, 0, 1, 0, 0],
+                          [1, 0, 0, 0, 0, 0, 0, 0],
+                          [1, 0, 0, 0, 0, 0, 1, 0],
+                          [0, 0, 1, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 1, 0, 0, 0],
+                          [0, 1, 0, 0, 0, 0, 0, 0]]
 
         for idx, x in enumerate(matrix_correct):
             for idj, j in enumerate(x):
--- BottleWebProject_C921_1_RRC/tests/converter/test_converter.py
+++ BottleWebProject_C921_1_RRC/tests/converter/test_converter.py
@@ -1,18 +1,27 @@
 import unittest
 
-from BottleWebProject_C921_1_RRC.modules.formatConverter import data_to_matrix, matrix_to_data
+from BottleWebProject_C921_1_RRC.modules.formatConverter import data_to_matrix
 
 
 class TestFileConverter(unittest.TestCase):
     def test_converting_both_ways(self):
-        with open("graph_default.json") as f:
-            json_in = f.read()
+        data = {'Nodes': [{'x': -53, 'y': -98, 'id': '0', 'connections': ['2', '1', '3', '4']},
+                          {'x': 80, 'y': -30, 'id': '1', 'connections': ['0', '2', '3', '4', '7']},
+                          {'x': -82, 'y': 22, 'id': '2', 'connections': ['0', '4', '1', '3', '5']},
+                          {'x': -3, 'y': -110, 'id': '3', 'connections': ['0', '4', '1', '2']},
+                          {'x': 49, 'y': 31, 'id': '4', 'connections': ['0', '3', '2', '1', '6']},
+                          {'x': -69, 'y': 179, 'id': '5', 'connections': ['2', '6', '7']},
+                          {'x': 65, 'y': 201, 'id': '6', 'connections': ['4', '5']},
+                          {'x': 43, 'y': 111, 'id': '7', 'connections': ['1', '5']}]}
+        matrix_correct = [[0, 1, 1, 1, 1, 0, 0, 0], [1, 0, 1, 1, 1, 0, 0, 1], [1, 1, 0, 1, 1, 1, 0, 0],
+                          [1, 1, 1, 0, 1, 0, 0, 0], [1, 1, 1, 1, 0, 0, 1, 0], [0, 0, 1, 0, 0, 0, 1, 1],
+                          [0, 0, 0, 0, 1, 1, 0, 0], [0, 1, 0, 0, 0, 1, 0, 0]]
+        matrix = data_to_matrix(data)
 
-        matrix = data_to_matrix(json_in)
-
-        json_out = matrix_to_data(matrix)
-
-        self.assertEqual(json_out, json_in)
+        for idx, x in enumerate(matrix_correct):
+            for idj, j in enumerate(x):
+                val = matrix[idx][idj]
+                self.assertEqual(j, val)
 
 
 if __name__ == '__main__':
